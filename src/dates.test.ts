d680cde25cafcf4f503cab7143e2e25c2760f9aa
Jag Reehal <jag.reehal@gmail.com>
2026-09-23 11:44:49 +0100

    fix: make startOfDayIso return UTC midnight, deterministic across host TZ, from review

--- src/dates.test.ts
+++ src/dates.test.ts
@@ -3,6 +3,6 @@ import { startOfDayIso } from "./dates";
 
 describe("startOfDayIso", () => {
   it("returns midnight at the start of the day", () => {
-    expect(startOfDayIso(2026, 7, 1)).toBe("2026-06-30T23:00:00.000Z");
+    expect(startOfDayIso(2026, 7, 1)).toBe("2026-07-01T00:00:00.000Z");
   });
 });
