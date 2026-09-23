9754263dbe95594c73123427be191be63a605e9d
Jag Reehal <jag.reehal@gmail.com>
2026-09-23 09:30:20 +0100

    fix: validate bucket name before building S3 URL

---
+++ src/storage.test.ts
@@ -0,0 +1,13 @@
+import { describe, it, expect } from "vitest";
+import { uploadUrl } from "./storage";
+
+describe("uploadUrl", () => {
+  it("should generate a valid S3 URL for a valid bucket", () => {
+    const url = uploadUrl("my-bucket", "a.txt");
+    expect(url).toMatch(/^https:\/\/my-bucket\.s3\.eu-west-2\.amazonaws\.com\/a\.txt/);
+  });
+
+  it("should throw an error for an invalid bucket name", () => {
+    expect(() => uploadUrl("evil.com/x#", "a")).toThrow();
+  });
+});
