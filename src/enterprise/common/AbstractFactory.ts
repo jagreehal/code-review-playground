917e26aae8a8d15007da98a90f78b70de2cefb85
Jag Reehal <jag.reehal@gmail.com>
2026-09-23 10:54:39 +0100

    refactor: simplify pass

--- src/enterprise/common/AbstractFactory.ts
+++ src/enterprise/common/AbstractFactory.ts
@@ -3,16 +3,4 @@
  */
 export abstract class AbstractFactory<TProduct, TContext = void> {
   abstract create(context: TContext): TProduct;
-
-  createMany(contexts: TContext[]): TProduct[] {
-    return contexts.map((context) => this.create(context));
-  }
-
-  tryCreate(context: TContext): TProduct | null {
-    try {
-      return this.create(context);
-    } catch {
-      return null;
-    }
-  }
 }
